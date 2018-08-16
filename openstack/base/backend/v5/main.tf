9051df8a1facd01b038e901975ca7b59bb67d8c9
Cloud user <cloud@rahul-lbaas.novalocal>
2018-08-16 11:00:53 +0000

    update input external_net key

--- openstack/base/backend/v5/main.tf
+++ openstack/base/backend/v5/main.tf
@@ -46,5 +46,5 @@ module "gateway" {
   router_name = "${var.gateway_name}"
   subnet_ids  = "${list(module.backend.subnet_id, module.bastion.subnet_id, module.lbaasv2.vip_subnet_id)}"
   nb_subnet   = "3"
-  ext_gw_net  = "${var.external_net}"
+  external_net  = "${var.external_net}"
 }
